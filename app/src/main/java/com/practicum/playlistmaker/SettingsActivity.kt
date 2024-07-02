781dea89d8c64e16b70b22fd778246dd563e393c
Annet Lovett <annamarkova2802@gmaol.com>
2024-07-02 22:45:39 +0400

    first commit

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,23 @@
+package com.practicum.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SettingsActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<Button>(R.id.buttonSettingsBack)
+
+        backButton.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+    }
+}
