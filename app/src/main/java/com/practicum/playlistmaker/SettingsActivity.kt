fa7a10bf95e74474ba41300ed037fb4e3846c8c3
Annet Lovett <annamarkova2802@gmaol.com>
2024-07-26 10:29:10 +0400

    buttons

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,8 +2,15 @@ package com.practicum.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
@@ -18,6 +25,35 @@ class SettingsActivity : AppCompatActivity() {
         backButton.setNavigationOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
+            finish()
         }
+
+        val darkThemeBtn = findViewById<Switch>(R.id.theme_switcher)
+
+        darkThemeBtn.setOnClickListener {
+
+            AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
+        }
+
+        val shareBtn = findViewById<Button>(R.id.btnShareSettings)
+
+        shareBtn.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND_MULTIPLE)
+        }
+
+        val writeBtn = findViewById<Button>(R.id.btnWriteToSupportSettings)
+
+        writeBtn.setOnClickListener {
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("annamarkova143@gmail.com"))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
+
     }
 }
