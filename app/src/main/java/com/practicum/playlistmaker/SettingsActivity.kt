f50c30981ffd9e698e5eea5620e452de71f8c538
Annet Lovett <annamarkova2802@gmaol.com>
2024-07-29 22:44:35 +0400

    проблема с цветом шрифтов

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,10 +2,8 @@ package com.practicum.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
@@ -31,14 +29,12 @@ class SettingsActivity : AppCompatActivity() {
         val darkThemeBtn = findViewById<Switch>(R.id.theme_switcher)
 
         darkThemeBtn.setOnClickListener {
-
             AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
         }
 
         val shareBtn = findViewById<Button>(R.id.btnShareSettings)
 
         shareBtn.setOnClickListener {
-
             val sendIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
                 putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/")
@@ -72,7 +68,5 @@ class SettingsActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
-
-
     }
 }
