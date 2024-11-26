751c4e0273b1b3e1c4791b7b5ce9ad62a5439672
Annet Lovett <annamarkova2802@gmaol.com>
2024-11-26 23:06:51 +0400

    api 1st version

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,32 +1,88 @@
 package com.practicum.playlistmaker
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
+    private val baseTrackUrl = BASE_URL
     private var inputValue: String = VALUE_DEF
 
-    @SuppressLint("MissingInflatedId")
+    lateinit var buttonBack: MaterialToolbar
+    lateinit var buttonClear: ImageButton
+    lateinit var searchInput: EditText
+    lateinit var recyclerTrack: RecyclerView
+    lateinit var nothingFound: LinearLayout
+    lateinit var serverpromlems: LinearLayout
+    lateinit var refreshButton: Button
+
+    private val listOfTracks = ArrayList<Track>()
+    private val trackAdapter = TrackListAdapter()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseTrackUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val trackService = retrofit.create(PlaylistApi::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_search)
 
-        val buttonBack = findViewById<MaterialToolbar>(R.id.buttonSettingsBack)
-        val buttonClear = findViewById<ImageButton>(R.id.buttonClear)
-        val searchInput = findViewById<EditText>(R.id.searchInput)
-        val recyclerTrack = findViewById<RecyclerView>(R.id.recyclerTrack)
+        buttonBack = findViewById(R.id.buttonSettingsBack)
+        buttonClear = findViewById(R.id.buttonClear)
+        searchInput = findViewById(R.id.searchInput)
+        recyclerTrack = findViewById(R.id.recyclerTrack)
+        nothingFound = findViewById(R.id.nothingFound)
+        serverpromlems = findViewById(R.id.serverProblems)
+        refreshButton = findViewById(R.id.refreshButtonSearch)
+
+        recyclerTrack.adapter = trackAdapter
+
+        trackAdapter.listOfTheTracks = listOfTracks
+
+        searchInput.addTextChangedListener(
+            onTextChanged = {s: CharSequence?, _, _, _ ->
+                    buttonClear.isVisible = !s.isNullOrEmpty()
+                    inputValue = s.toString()
+        })
+
+        searchInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                nothingFound.visibility = View.GONE
+                enreachAndViewTracks()
+            }
+            false
+        }
+
+        refreshButton.setOnClickListener {
+            serverpromlems.visibility = View.GONE
+            listOfTracks.clear()
+            searchInput.text.clear()
+        }
+
+        buttonClear.setOnClickListener {
+            searchInput.text.clear()
+        }
 
         buttonBack.setNavigationOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
@@ -34,33 +90,88 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        searchInput.addTextChangedListener(
+    }
 
-            onTextChanged = {s: CharSequence?, _, _, _ ->
-                    buttonClear.isVisible = !s.isNullOrEmpty()
-                    recyclerTrack.isVisible = !s.isNullOrEmpty()
-                    inputValue = s.toString()
-        })
+    private fun enreachAndViewTracks() {
+//        listOfTracks.clear();
+//        trackService.search(inputValue)
+//            .enqueue(object : Callback<TrackResponse> {
+//                override fun onResponse(
+//                    call: Call<TrackResponse>,
+//                    response: Response<TrackResponse>,
+//                ) {
+//                    if (response.body() != null && response.code() == 200) {
+//                        listOfTracks.addAll(response.body()?.results!!.toList())
+//                        trackAdapter.notifyDataSetChanged()
+//                    } else {
+//                        TODO("")
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+//                    TODO("")
+//                }
+//            })
 
-        buttonClear.setOnClickListener {
-            searchInput.text.clear()
+        if (inputValue.isNotEmpty()) {
+            trackService.search(inputValue).enqueue(object :
+                Callback<TrackResponse> {
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>
+                ) {
+                    if (response.code() == 200) {
+                        listOfTracks.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            listOfTracks.addAll(response.body()?.results!!)
+                            trackAdapter.notifyDataSetChanged()
+                        }
+                        if (listOfTracks.isEmpty()) {
+                            showMessage(NOTHING_FOUND, "")
+                        }
+                    } else {
+                        showMessage(SERVER_PROBLEMS, response.code().toString())
+
+
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    showMessage(SERVER_PROBLEMS, t.message.toString())
+                }
+
+            })
+        }
+    }
+
+    private fun showMessage(text: String, additionalMessage: String) {
+        if (text == NOTHING_FOUND) {
+            nothingFound.visibility = View.VISIBLE
+            listOfTracks.clear()
+            trackAdapter.notifyDataSetChanged()
+
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
         }
 
-        val listOfTracks = arrayListOf(
-            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
-        )
+        if (text == SERVER_PROBLEMS) {
+            serverpromlems.visibility = View.VISIBLE
+            listOfTracks.clear()
+            trackAdapter.notifyDataSetChanged()
 
-        val listOfTracksAdapter = TrackListAdapter(listOfTracks)
-
-        val trackRecycler = findViewById<RecyclerView>(R.id.recyclerTrack)
-
-        trackRecycler.adapter = listOfTracksAdapter
-
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                nothingFound.visibility = View.GONE
+                serverpromlems.visibility = View.GONE
+            }
+        }
     }
+
+
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -70,6 +181,9 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         const val INPUT_AMOUNT = "INPUT_AMOUNT"
         const val VALUE_DEF = ""
+        const val BASE_URL = "https://itunes.apple.com"
+        const val NOTHING_FOUND = "nothing_found"
+        const val SERVER_PROBLEMS = "server_problems"
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
