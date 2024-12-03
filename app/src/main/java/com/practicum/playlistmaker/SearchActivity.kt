df3027abf0ccb77af96a8a41db63851cb01bded2
Annet Lovett <annamarkova2802@gmaol.com>
2024-12-03 11:04:58 +0400

    api changes for the first review

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,6 +1,4 @@
 package com.practicum.playlistmaker
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,8 +7,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.RecyclerView
@@ -87,33 +83,12 @@ class SearchActivity : AppCompatActivity() {
         }
 
         buttonBack.setNavigationOnClickListener {
-            val displayIntent = Intent(this, MainActivity::class.java)
-            startActivity(displayIntent)
             finish()
         }
 
     }
 
     private fun enreachAndViewTracks() {
-//        listOfTracks.clear();
-//        trackService.search(inputValue)
-//            .enqueue(object : Callback<TrackResponse> {
-//                override fun onResponse(
-//                    call: Call<TrackResponse>,
-//                    response: Response<TrackResponse>,
-//                ) {
-//                    if (response.body() != null && response.code() == 200) {
-//                        listOfTracks.addAll(response.body()?.results!!.toList())
-//                        trackAdapter.notifyDataSetChanged()
-//                    } else {
-//                        TODO("")
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
-//                    TODO("")
-//                }
-//            })
 
         if (inputValue.isNotEmpty()) {
             trackService.search(inputValue).enqueue(object :
@@ -130,11 +105,11 @@ class SearchActivity : AppCompatActivity() {
                             trackAdapter.notifyDataSetChanged()
                         }
                         if (listOfTracks.isEmpty()) {
-                            showMessage(NOTHING_FOUND, "")
+                            showMessage(NOTHING_FOUND)
                         }
                     } else {
                         recyclerTrack.visibility = View.GONE
-                        showMessage(SERVER_PROBLEMS, response.code().toString())
+                        showMessage(SERVER_PROBLEMS)
 
 
                     }
@@ -142,37 +117,25 @@ class SearchActivity : AppCompatActivity() {
 
                 override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
                     recyclerTrack.visibility = View.GONE
-                    showMessage(SERVER_PROBLEMS, t.message.toString())
+                    showMessage(SERVER_PROBLEMS)
                 }
 
             })
         }
     }
 
-    private fun showMessage(text: String, additionalMessage: String) {
+    private fun showMessage(text: String) {
         if (text == NOTHING_FOUND) {
             nothingFound.visibility = View.VISIBLE
             listOfTracks.clear()
             trackAdapter.notifyDataSetChanged()
 
-            if (additionalMessage.isNotEmpty()) {
-                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
-                    .show()
-            }
         }
 
         if (text == SERVER_PROBLEMS) {
             serverpromlems.visibility = View.VISIBLE
             listOfTracks.clear()
             trackAdapter.notifyDataSetChanged()
-
-            if (additionalMessage.isNotEmpty()) {
-                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
-                    .show()
-            } else {
-                nothingFound.visibility = View.GONE
-                serverpromlems.visibility = View.GONE
-            }
         }
     }
 
