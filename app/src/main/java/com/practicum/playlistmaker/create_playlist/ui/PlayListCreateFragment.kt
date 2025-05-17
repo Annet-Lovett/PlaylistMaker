be1fca51cd325efa2cecb816e909f6e13f76f75c
Annet <annamrkova2802@gmail.com>
2025-05-17 23:49:33 +0400

    playlist create layout

---
+++ app/src/main/java/com/practicum/playlistmaker/create_playlist/ui/PlayListCreateFragment.kt
@@ -0,0 +1,10 @@
+package com.practicum.playlistmaker.create_playlist.ui
+
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmaker.R
+
+class PlayListCreateFragment: Fragment(R.layout.fragment_playlist_create) {
+
+
+
+}
