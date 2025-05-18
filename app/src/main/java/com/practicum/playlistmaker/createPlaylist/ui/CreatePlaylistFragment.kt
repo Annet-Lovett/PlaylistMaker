653de50bd4bec6c937a80fd7c1ebeb9322cf19ae
Annet <annamrkova2802@gmail.com>
2025-05-18 15:31:52 +0400

    playlist create connect

---
+++ app/src/main/java/com/practicum/playlistmaker/createPlaylist/ui/CreatePlaylistFragment.kt
@@ -0,0 +1,8 @@
+package com.practicum.playlistmaker.createPlaylist.ui
+
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmaker.R
+
+class CreatePlaylistFragment: Fragment(R.layout.fragment_playlist_create) {
+
+}
--- app/src/main/java/com/practicum/playlistmaker/main/ui/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/main/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.practicum.playlistmaker.main.ui
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.NavigationRail
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
 import com.practicum.playlistmaker.R
@@ -16,8 +19,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         val navController = Navigation.findNavController(this, R.id.content)
         binding.bottomNav.setupWithNavController(navController)
     }
