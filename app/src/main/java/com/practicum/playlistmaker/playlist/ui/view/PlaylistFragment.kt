7d533a96dfe631677144fe1ec25258b6c4aaa580
Annet <annamrkova2802@gmail.com>
2025-05-27 22:23:00 +0400

    share playlist

--- app/src/main/java/com/practicum/playlistmaker/playlist/ui/view/PlaylistFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/playlist/ui/view/PlaylistFragment.kt
@@ -1,10 +1,12 @@
 package com.practicum.playlistmaker.playlist.ui.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
@@ -18,6 +20,8 @@ import com.practicum.playlistmaker.search.ui.view.SearchTrackListAdapter
 import com.practicum.playlistmaker.sharing.domain.models.Track
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
+import java.text.SimpleDateFormat
+import java.util.Locale
 import kotlin.getValue
 
 class PlaylistFragment : Fragment() {
@@ -63,6 +67,17 @@ class PlaylistFragment : Fragment() {
 
         binding.playerBackButton.setNavigationOnClickListener {
             findNavController().popBackStack()
+        }
+
+        binding.playlistButtonShare.setOnClickListener{
+
+            if (playlistViewModel.playlistLiveData.value!!.listOfTheTracks.isEmpty()){
+                emptyTracksError()
+            }
+            else {
+                sharePlaylist(playlistViewModel.playlistLiveData.value!!)
+            }
+
         }
 
     }
@@ -116,6 +131,41 @@ class PlaylistFragment : Fragment() {
                 playlistViewModel.deleteTrack(track)
             }
             .show()
+    }
+
+    private fun emptyTracksError() {
+
+        Toast.makeText(requireContext(),
+            "В этом плейлисте нет списка треков, которым можно поделиться",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    private fun sharePlaylist(viewState: PlaylistViewState) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, getPlaylistAsText(viewState))
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
+    private fun getPlaylistAsText(viewState: PlaylistViewState): String {
+        var playlistText = viewState.nameOfThePlaylist + "\n"
+        playlistText += viewState.description + "\n"
+        playlistText +=  viewState.numberOfTheTracks.padStart(2, '0') + " треков" + "\n"
+
+        viewState.listOfTheTracks.forEachIndexed { index, track ->
+            playlistText += "${index+1}. " + getTrackAsText(track)  + "\n"
+        }
+
+        return playlistText
+    }
+
+    private fun getTrackAsText(track: Track): String {
+        val duration = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis.toLong())
+        return "${track.artistName} - ${track.trackName} ($duration)"
     }
 
 }
